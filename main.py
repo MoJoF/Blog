58d0cf4719cc65cbbb59a4c4dc005919767e8f58
MoJoF <mojo.13081998@gmail.com>
2020-09-05 13:23:43 +0300

    Добавление статьи и успешное сообщение при добавление.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Article:
+class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     text = db.Column(db.Text, nullable=False)
@@ -23,9 +23,29 @@ def news():
     return render_template("news.html")
 
 
-@app.route('/create')
+@app.route('/create', methods=['GET', 'POST'])
 def create():
-    return render_template("create.html")
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+
+        article = Article(title=title, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/success')
+
+        except:
+            return "Error"
+
+    else:
+        return render_template("create.html")
+
+@app.route('/success')
+def success_add():
+    return render_template("success.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
