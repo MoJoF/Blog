f049b8b0fe2b41027a0b895e94c9c8facd3cd87b
MoJoF <mojo.13081998@gmail.com>
2020-09-05 12:53:14 +0300

    Доработка base, добавление БД и доработка шаблона новостей

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Article:
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return "<Article %r>" % self.id
 
 @app.route('/')
 def index():
